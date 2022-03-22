25d3ecb1b1d78e0a9f5f414b4277076ad4fa8edc
IvsonEmidio <80026873+IvsonEmidio@users.noreply.github.com>
2022-03-22 19:02:50 -0300

    Uncommented all fixtures, all are passing.

--- tests/Feature/API/V1/Cart/CartDiscountTest.php
+++ tests/Feature/API/V1/Cart/CartDiscountTest.php
@@ -47,14 +47,15 @@ class CartDiscountTest extends TestCase
 
         // These ones you need to make pass.
         // Uncomment the lines, one by one, so that it's easier to implement :)
-//        yield from $this->getFixture('valid_with_discount_above_3000');
-//        yield from $this->getFixture('valid_with_discount_take_3_pay_2');
-//        yield from $this->getFixture('valid_with_discount_take_3_pay_2_multiple_times');
-//        yield from $this->getFixture('valid_with_discount_for_same_category');
-//        yield from $this->getFixture('valid_with_discount_for_same_category_same_price');
-//        yield from $this->getFixture('valid_with_discount_for_employee');
-//        yield from $this->getFixture('valid_with_discount_for_new_customer');
-//        yield from $this->getFixture('valid_with_only_the_biggest_discount');
+        yield from $this->getFixture('valid_with_discount_above_3000');
+        yield from $this->getFixture('valid_with_discount_take_3_pay_2');
+        yield from $this->getFixture('valid_with_discount_take_3_pay_2_multiple_times');
+        yield from $this->getFixture('valid_with_discount_for_same_category');
+        yield from $this->getFixture('valid_with_discount_for_same_category_same_price');
+
+        yield from $this->getFixture('valid_with_discount_for_employee');
+        yield from $this->getFixture('valid_with_discount_for_new_customer');
+        yield from $this->getFixture('valid_with_only_the_biggest_discount');
     }
 
     /**
